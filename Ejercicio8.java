c1039a8cf40911ecbf5fda9fd1baf1e398bd37ee
Manuel Rojas <manuerjs70@gmail.com>
2021-06-08 23:08:09 -0600

    Agregue codigo en java sobre el ejercicio 18

---
+++ Ejercicio8.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/*
+202030799 Manuel Rojas
+Diseñar un algoritmo que, dados tres n�meros
+enteros, calcule e imprima el promedio entre ellos.
+*/
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        int num1, num2, num3;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el primer numero: ");
+        num1 = scanner.nextInt();
+        System.out.print("Ingrese el segundo numero: ");
+        num2 = scanner.nextInt();
+        System.out.print("Ingrese el tercer numero: ");
+        num3 = scanner.nextInt();
+        int promedio = (num1 + num2 + num3)/3
+        System.out.println("El promedio de los numeros ingresados es: " + promedio);
+
+        
+    }
+    
+}
