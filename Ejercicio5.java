4538142b874c3d62af41bab05cea690701d973ab
Manuel Rojas <manuerjs70@gmail.com>
2021-06-08 19:08:15 -0600

    Modifique solicitud de datos en ejercicio5

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -8,7 +8,7 @@ public class Ejercicio5 {
     public static void main(String[] args) {
         int num, siguiente;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese un numero");
+        System.out.print("Ingrese un numero: ");
         num = scanner.nextInt();
         siguiente = num + 1;
         System.out.println("El numero que le sigue a " + num + " es: " + siguiente); 
