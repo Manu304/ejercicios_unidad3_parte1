694dc599615ced568e2d45462a18ada1fb2fe24a
Manuel Rojas <manuerjs70@gmail.com>
2021-06-08 22:50:53 -0600

    Quite variables innecesarias

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 /*
 202030799 Manuel Rojas
 Dise�ar un algoritmo que genere un n�meroaleatorio del 0 al 200, 
@@ -7,7 +5,6 @@ lo muestre y luego calcule y muestre el mismo n�mero aumentado en un 30%.
 */
 public class Ejercicio6 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         int aleatorio = 0;
         aleatorio = (int) Math.floor(Math.random()*(200+1));
         System.out.println("El numero generado es: " + aleatorio);
